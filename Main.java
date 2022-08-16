6dfc02fc8266bfc5f509cd2837292edd27b169df
Pedro Q <96138011+pequepo@users.noreply.github.com>
2022-08-16 14:22:16 +0200

    first commit

---
+++ Main.java
@@ -0,0 +1,38 @@
+public class Main {
+    
+    public static void main(String[] args) {
+        Persona persona = new Persona();
+        persona.setNombre("Pedro");
+        System.out.println("Mi nombre es " + persona.getNombre());
+        persona.setEdad(20);
+        System.out.println("Tengo" + persona.getEdad() + "años");
+        persona.setTelefono(123456789);
+        System.out.println("Mi telefono es" + persona.getTelefono());
+
+    }
+ 
+class Persona {
+    private String nombre; 
+    private int edad;
+    private int telefono;
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre; 
+    }
+    public String getNombre(){
+        return nombre; 
+    }
+    public void setEdad(int edad) {
+        this.edad = edad; 
+    }
+    public int getEdad() {
+        return edad;
+    }
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+    public int getTelefono() {
+        return telefono;
+    }
+}
+}
